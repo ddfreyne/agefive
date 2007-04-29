f3f9bbfd74b3686e1534211bde6f113bc13c2d44
chucker23n <chucker23n@93925bee-112e-0410-9d97-c17d952d65b0>
2007-04-29 20:17:46 +0000

    Adding incomplete Resource class, and some TODO notes

---
+++ Backend/Resource.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# AgeFive
+# Modernizing Riven in Ruby
+#
+# Resource
+#
+# Created by Sören Nils Kuklau on 2007-04-29.
+# Copyright (c) 2007. Some rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# The full license can be retrieved at:
+# http://www.opensource.org/licenses/mit-license.php
+#
+
+class Resource
+	attr_reader(:type, :resourceID)
+
+	def initialize(type, resourceID, fileTableIndex)
+		@type = type
+		@resourceID = resourceID
+		@fileTableIndex = fileTableIndex
+	end
+
+	def name
+		# how do we do that?
+	end
+end
